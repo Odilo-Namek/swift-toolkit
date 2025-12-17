2f380c63f371945a453d82170ba2df0830cb378a
Alvaro Franco <afranco@odilo.us>
2025-12-17 15:57:01 +0100

    Update dependency version constraint

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/readium/Fuzi.git", from: "3.1.4"),
         // From 1.6.0, the build fails in GitHub actions with Carthage
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", "1.5.1" ..< "1.6.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.0"),
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.2"),
         .package(url: "https://github.com/ra1028/DifferenceKit.git", from: "1.3.0"),
         .package(url: "https://github.com/readium/GCDWebServer.git", from: "4.0.0"),
